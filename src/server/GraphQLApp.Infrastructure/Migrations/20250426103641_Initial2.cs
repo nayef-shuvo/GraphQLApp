b0e801713969ab001e6f8f7a429083b44bfdaf56
Nayef Al Mahbub <nayef.mahbub@brainstation-23.com>
2025-05-24 14:18:32 +0600

    Repository restructure

---
+++ src/server/GraphQLApp.Infrastructure/Migrations/20250426103641_Initial2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQLApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Passwords",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passwords_UserId",
+                table: "Passwords",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Passwords_Users_UserId",
+                table: "Passwords",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Passwords_Users_UserId",
+                table: "Passwords");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Passwords_UserId",
+                table: "Passwords");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Passwords");
+        }
+    }
+}
---
+++ src/server/GraphQLApp.Web/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+using GraphQLApp.Entities;
+using GraphQLApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphQLApp.Controllers;
+
+[Route("auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+   
+}
